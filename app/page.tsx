0712bf81b212b7d3437019bee38480377a4c003a
bennkemboi <benkemboi23@gmail.com>
2023-12-22 13:18:08 +0300

    add product card

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Container from "@/components/container";
 import HomeBanner from "@/components/homeBanner";
+import ProductCard from "@/components/productCard";
 import { products } from "@/utils/products";
 import { truncateText } from "@/utils/truncateText";
 
@@ -10,9 +11,9 @@ export default function Home() {
         <div>
           <HomeBanner />
         </div>
-        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+        <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {products.map((product: any) => {
-            return <div>{truncateText(product.name)} </div>;
+            return <ProductCard data={product} />;
           })}
         </div>
       </Container>
--- components/productCard.tsx
+++ components/productCard.tsx
@@ -1,6 +1,31 @@
-const ProductCard = () => {
-    return <div>
+"use client";
 
-    </div>
+import { truncateText } from "@/utils/truncateText";
+import Image from "next/image";
+
+interface ProductCardProps {
+  data: any;
+  name: any;
 }
-export default ProductCard
+
+const ProductCard: React.FC<ProductCardProps> = ({ data }) => {
+  return (
+    <div className="col-span-1 cursor-pointer border-[1.2px] border-slate-200 bg-slate-50 rounded-sm p-2 transition hover:scale-105 text-center text-sm">
+      <div className="flex flex-col items-center w-full gap-1 ">
+        <div className="aspect-square overflow-hidden relative w-full ">
+          <Image
+            fill
+            src={data.images[0].image}
+            alt={data.name}
+            className="w-full h-full object-contain"
+          />
+        </div>
+        <div className="mt-4">{truncateText(data.name)}</div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+export default ProductCard;
