095efd4df291c78a69eee4b0c6ca0f703fe81f6a
bennkemboi <benkemboi23@gmail.com>
2024-01-17 15:29:01 +0300

    update product card changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Container from "@/components/container";
 import HomeBanner from "@/components/homeBanner";
 import ProductCard from "@/components/productCard";
 import { products } from "@/utils/products";
-import { truncateText } from "@/utils/truncateText";
 
 export default function Home() {
   return (
@@ -13,6 +12,7 @@ export default function Home() {
         </div>
         <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-8">
           {products.map((product: any) => {
+            // eslint-disable-next-line react/jsx-key
             return <ProductCard data={product} />;
           })}
         </div>
