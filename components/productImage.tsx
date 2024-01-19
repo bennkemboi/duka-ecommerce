8c9f2c26b9a30547eea5f1fd3d567e7b81501832
bennkemboi <benkemboi23@gmail.com>
2024-01-19 17:41:09 +0300

    update productimage

--- components/productImage.tsx
+++ components/productImage.tsx
@@ -40,8 +40,13 @@ const ProductImage: React.FC<ProductImageProps> = ({
         })}
       </div>
       <div className="col-span-5 relative aspect-square  ">
-        <Image fill className="w-full
-         h-full object-contain max-h-[500px] min-h-[300px] sm:min-h-[400px] " />
+        <Image
+          fill
+          src={cartProduct.selectedImg.image}
+          alt={cartProduct.name}
+          className="w-full
+         h-full object-contain max-h-[500px] min-h-[300px] sm:min-h-[400px] "
+        />
       </div>
     </div>
   );
